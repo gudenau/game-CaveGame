ff598ebf290795601cda5751df2700fee86af97e
gudenau <gudenau@users.noreply.github.com>
2024-04-02 20:37:12 -0500

    Added README

--- modules/launcher/src/main/java/net/gudenau/cavegame/launcher/Launcher.java
+++ modules/launcher/src/main/java/net/gudenau/cavegame/launcher/Launcher.java
@@ -5,6 +5,7 @@ import net.gudenau.cavegame.logger.Logger;
 public class Launcher {
     private static final Logger LOGGER = Logger.forName("launcher");
 
+    //TODO Implement all of this
     public static void main(String[] args) {
         LOGGER.info("Hello world!");
         LOGGER.info(Launcher.class.getModule().getName());
